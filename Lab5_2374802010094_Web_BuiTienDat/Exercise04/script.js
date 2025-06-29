7d1224c59f2159eed773468ffe3680e65fad4194
BuiTienDat2004 <btiendat204@gmail.com>
2025-06-29 20:57:33 +0700

    Add files via upload

---
+++ Lab5_2374802010094_Web_BuiTienDat/Exercise04/script.js
@@ -0,0 +1,5 @@
+function triple(x) {
+  return x * 3;
+}
+
+console.log(triple(10)); // Output: 30
---
+++ Lab5_2374802010094_Web_BuiTienDat/Exercise10/script.js
@@ -0,0 +1,7 @@
+const user = { firstName: "Kieu", lastName: "Trang" };
+
+const { firstName, lastName } = user;
+
+const greeting = `Hello, ${firstName} ${lastName}!`;
+
+console.log(greeting); // Output: Hello, Kieu Trang!
